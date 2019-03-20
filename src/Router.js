69d628b6d8c7b8d3aaf24055af4ff0360db1c418
Brandon Turner <brandontheturner@gmail.com>
2019-03-19 23:42:39 -0500

    working on positioning component links across page

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,18 @@
 import React from "react";
-import Switch from "react-router-dom";
-import Route from "react-router-dom";
+import { Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 import App from "./App";
+import Home from "./components/Home";
+import About from "./components/About";
+
 //import home and about
 
-const Router = () => {
+const Router = () => (
   <Switch>
     <Route exact path="/" component={App} />
     <Route path="/home" component={Home} />
     <Route path="/about" component={About} />
-  </Switch>;
-};
+  </Switch>
+);
 
 export default Router;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+const navStyle = {
+  display: "flex",
+  // textAlign: "center"
+  justifyContent: "center",
+  margin: "30px"
+};
 
 const Header = () => (
-  <div className="header">
-    <p>this is the header</p>
+  <div className="header" style={navStyle}>
+    <ul>
+      <Link to="/">Landing</Link>
+      <Link to="/home">Home</Link>
+      <Link to="/about">About</Link>
+    </ul>
   </div>
-)
+);
 
-export default Header
+export default Header;
